6ee60119c80904a7c232849b1eca6cb6732c9fda
Seth Bashford <seth.bashford@boystown.org>
2024-10-03 13:40:48 -0500

    trying to understand how to jar this project

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -eu
+shopt -s globstar
 
-javac -cp "src:lib/*" ./src/edu/cmu/cs/eyetrack/gui/EyeTrack.java
-jar cvfm out.jar manifest.txt -C src . lib/*
+javac -cp "src:lib/*" ./src/**/*.java -d bin
+for FILE in images.jar jcalendar-1.3.3.jar joda-time-1.6.2.jar looks-2.0.1.jar opencsv-2.2.jar timingframework-swing-3.0.jar; do
+    (cd bin/ && jar xf ../lib/$FILE)
+done
+rm -r bin/META-INF/
+jar cvfe TrackIt.jar edu.cmu.cs.eyetrack.gui.EyeTrack -C bin .
