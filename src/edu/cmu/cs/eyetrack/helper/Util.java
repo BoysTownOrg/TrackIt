952e1dde37273f768b682eb7f96d8115cab5fac9
Seth Bashford <seth.bashford@boystown.org>
2024-11-27 09:22:20 -0600

    bump version

--- src/edu/cmu/cs/eyetrack/helper/Util.java
+++ src/edu/cmu/cs/eyetrack/helper/Util.java
@@ -8,7 +8,7 @@ public abstract class Util {
 	public static enum PanelID { START_MENU, GAME, BUFFER1, DISTRACTOR_LINEUP, BUFFER2 };
 
 	public static String getTrackItVersion() {
-		return "1.0.0-variable-trial-length";
+		return "1.0.1-variable-trial-length";
 	}
 	
 	// Debug settings, debug utilities
